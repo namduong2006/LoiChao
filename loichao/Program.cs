024bf7d77788bee62dad37990df55278b7759a21
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-02 16:00:50 +0700

    Add project files.

---
+++ loichao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loichao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string ten;
+            Console.WriteLine("Enter your name:");
+            ten= Console.ReadLine();
+            Console.WriteLine("Hello: " +ten);
+            Console.ReadLine();
+        }
+    }
+}
